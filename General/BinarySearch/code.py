d419a8c00d8fb466c9ab2121e6e37a90891a54e2
Mohammed Jasam <mnqnd@mst.edu>
2017-10-08 16:12:07 -0500

    Binary Search implemented

---
+++ General/BinarySearch/code.py
@@ -0,0 +1,22 @@
+a = [1,23,5,4,65,7,9,8,6,3,2,10,11,22,3235,67,8,3,3,1,23,32,5,34,7568,785,65,345,234]
+
+a = sorted(list(set(a)))
+print(a)
+
+find = 1
+
+def searchh(a, find):
+    size = len(a)
+    mid = round(size/2)
+    print(a[:mid-1],mid, a[mid:])
+    if a[mid] == find:
+        print("found " + str(a[mid]))
+    else:
+        if find < a[mid]:
+            a = a[:mid-1]
+            searchh(a, find)
+        else:
+            a = a[mid:]
+            searchh(a, find)
+
+searchh(a, find)
