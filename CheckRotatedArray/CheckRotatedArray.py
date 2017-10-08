5ef24f6ed3bcaf890502cbbad62ca879cbd16473
Mohammed Jasam <mnqnd@mst.edu>
2017-10-08 15:12:08 -0500

    Check if array is rotated or not

---
+++ CheckRotatedArray/CheckRotatedArray.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+print("Enter the size of Array")
+sizeA = int(input())
+arrayA = []
+for i in range(sizeA):
+    arrayA.append(int(input()))
+
+print("Enter the size of Array")
+sizeB = int(input())
+arrayB = []
+for i in range(sizeB):
+    arrayB.append(int(input()))
+
+if sizeA != sizeB:
+    print("No")
+
+if set(arrayA).difference(arrayB):
+    print("No")
+
+arrayA, arrayB = deque(arrayA), deque(arrayB)
+
+ans = False
+for i in range(sizeA):
+    arrayA.rotate()
+
+    if arrayA == arrayB:
+       ans = True
+
+if ans == True:
+    print("yes")
+
+
