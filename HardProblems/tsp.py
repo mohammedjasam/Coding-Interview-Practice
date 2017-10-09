98ec5202c92da5519558a4c8b1eb51c3b58028a5
Mohammed Jasam <mnqnd@mst.edu>
2017-10-08 22:08:01 -0500

    Made some tweaks

--- HardProblems/tsp.py
+++ HardProblems/tsp.py
@@ -1,13 +1,12 @@
 from collections import deque
 
-
+## THIS CODE PERMUTES!!!
 def toString(List):
     return ''.join(List)
 li = []
 def permute(a, l, r): # starting and ending indices
     if l==r:
         li.append(toString(a))
-        # print(toString(a))
     else:
         for i in range(l,r+1):
             a[l], a[i] = a[i], a[l]
@@ -15,6 +14,7 @@ def permute(a, l, r): # starting and ending indices
             a[l], a[i] = a[i], a[l] # backtrack
     return li
 
+## DATA
 dist = {
         1:[(2,10),(3,15),(4,20)],
         2:[(1,10),(3,35),(4,25)],
@@ -22,11 +22,14 @@ dist = {
         4:[(1,20),(2,25),(3,30)]
         }
 
-string = "1234"
+
+
+string = "1234" ## Four cities in the graph
 n = len(string)
 a = list(string)
-li = permute(a, 0, n-1)
+li = permute(a, 0, n-1) # Permuted list
 
+## Calculates the distance between 2 points A-B
 def retDist(a, b):
     x = dict(dist[a])
     return x[b]
@@ -42,12 +45,13 @@ def sendPattern(n):
         summ += retDist(a[i], b[i])
     return summ
 
-    # print(list(a),list(b))
 mini = 0
 result = []
+
+# For every permutation in the list, find the distance between points and track the smallest
 for x in li:
     n = [int(j) for j in x]
     res = sendPattern(n)
     result.append((res,n))
 
-print(min(result))
+print(min(result)) # Print the smallest result
